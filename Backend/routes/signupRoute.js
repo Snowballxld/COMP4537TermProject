1045d6ffbc9e46b2b7de018f0ef34b26374ac78e
Richard Li <rli108@my.bcit.ca>
2025-03-06 20:31:17 -0800

    Remove password length limitation, and redirection to home page after login

--- Backend/routes/signupRoute.js
+++ Backend/routes/signupRoute.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
     // Check if passwords match
     if (req.body.password !== req.body.confirmPassword) {
         console.log('Passwords do not match');
-        return res.render('signup', { error: 'Passwords do not match' });
+        return res.redirect('/signup.html?error=Passwords do not match');       
     }
 
     try {
@@ -26,7 +26,7 @@ router.post('/', async (req, res) => {
         const existingEmail = await User.findOne({ email: req.body.email });
         if (existingEmail) {
             console.log('Email already exists');
-            return res.render('signup', { error: 'Email already exists' });
+            return res.redirect('/signup.html?error=Email already exists');
         }
 
         // Create a new user with the provided information
@@ -35,13 +35,14 @@ router.post('/', async (req, res) => {
             password: req.body.password,
         });
         await user.save();
+        req.session.isLoggedIn = true;
         req.session.email = req.body.email;
 
         // Redirect to the user's profile page after successful signup
-        res.redirect(`/home`);
+        res.redirect('/home');
     } catch (err) {
         console.error('Signup error:', err);
-        res.render('signup', { error: 'An error occurred during signup' });
+        res.redirect('/signup.html?error=An error occurred during signup');
     }
 });
 
