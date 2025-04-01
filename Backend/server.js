a1a8f72efca11d935429d78f203f50a3027a2ad6
tysonchan1 <tyson95577@gmail.com>
2025-04-01 12:56:11 -0700

    reverted changes

--- Backend/server.js
+++ Backend/server.js
@@ -21,19 +21,12 @@ const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'swagger
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // I moved this up
-// app.use(cors({
-//     origin: "https://4537projectfrontend.netlify.app",
-//     credentials: true, // Ensure cookies are sent
-//     methods: "GET,POST,PUT,DELETE,OPTIONS",
-//     allowedHeaders: ["Content-Type", "Authorization"]
-// }));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://4537projectfrontend.netlify.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+app.use(cors({
+    origin: "https://4537projectfrontend.netlify.app",
+    credentials: true, // Ensure cookies are sent
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 // I just added this
