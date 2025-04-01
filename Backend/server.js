164d1e1b1fdd1b84c4481d5f2be8a8fe44781112
tysonchan1 <tyson95577@gmail.com>
2025-04-01 12:47:21 -0700

    oops pushed wrong link

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 //     allowedHeaders: ["Content-Type", "Authorization"]
 // }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header("Access-Control-Allow-Origin", "https://4537projectfrontend.netlify.app");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
